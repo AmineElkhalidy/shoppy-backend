002497c5066c9db2d5f86ee8b8d16d4b593fdfd7
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-10-30 16:18:25 +0100

    Pushing...

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,9 @@
 import NextAuth, { getServerSession } from "next-auth";
-
-// MongoDB Adapter
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-
-// Client Promise
 import clientPromise from "@/lib/mongodb";
-
-// Google Provider
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -20,7 +13,7 @@ export const authOptions = {
   // Will be responsible for re-using an active connection to db without making a request again!
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: ({ session, token, user }) => {
+    session: async ({ session, token, user }) => {
       if (session?.user?.email) {
         return session;
       } else {
